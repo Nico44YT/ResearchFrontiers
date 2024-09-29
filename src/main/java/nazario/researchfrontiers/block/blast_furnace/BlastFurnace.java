cdb510afe4648c38f8d1f5e15711770bf13679ce
Nico44YT <Nico_44-YouTube@gmx.de>
2024-09-29 16:01:36 +0200

    Made changes to BlastFurnace

--- src/main/java/nazario/researchfrontiers/block/blast_furnace/BlastFurnace.java
+++ src/main/java/nazario/researchfrontiers/block/blast_furnace/BlastFurnace.java
@@ -1,6 +1,9 @@
 package nazario.researchfrontiers.block.blast_furnace;
 
 import com.mojang.serialization.MapCodec;
+import com.teamresourceful.resourcefullib.client.highlights.base.Highlight;
+import com.teamresourceful.resourcefullib.client.highlights.base.Highlightable;
+import nazario.researchfrontiers.ResearchFrontiers;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,12 +11,32 @@ import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.stream.Stream;
+
 public class BlastFurnace extends BlockWithEntity {
+
+    public static final VoxelShape SHAPE = Stream.of(
+            Block.createCuboidShape(3, 0, 3, 13, 13, 13),
+            Block.createCuboidShape(13, 0, 3, 16, 13, 13),
+            Block.createCuboidShape(0, 0, 3, 3, 13, 13),
+            Block.createCuboidShape(3, 0, 0, 13, 13, 3),
+            Block.createCuboidShape(3, 0, 13, 13, 13, 16),
+            Block.createCuboidShape(0, 0, 0, 16, 1, 16),
+            Block.createCuboidShape(1, 0, 1, 3, 13, 3),
+            Block.createCuboidShape(13, 0, 1, 15, 13, 3),
+            Block.createCuboidShape(1, 0, 13, 3, 13, 15),
+            Block.createCuboidShape(13, 0, 13, 15, 13, 15)
+    ).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, BooleanBiFunction.OR)).get();
+
     public static final MapCodec<BlastFurnace> CODEC = createCodec(BlastFurnace::new);
 
 
@@ -39,10 +62,9 @@ public class BlastFurnace extends BlockWithEntity {
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!world.isClient) return ActionResult.CONSUME;
         if (player.isSneaking()) {
             // Toggle the OPEN state when the player is sneaking
-            world.setBlockState(pos, state.with(OPEN, BlastFurnaceState.OPEN));
+            world.setBlockState(pos, state.cycle(OPEN));
             return ActionResult.SUCCESS;
         }
         return ActionResult.PASS;
@@ -56,6 +78,11 @@ public class BlastFurnace extends BlockWithEntity {
     }
 
     @Override
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return SHAPE;
+    }
+
+    @Override
     protected BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
     }
@@ -63,5 +90,10 @@ public class BlastFurnace extends BlockWithEntity {
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
         builder.add(OPEN);
+    }
+
+    @Override
+    protected float getAmbientOcclusionLightLevel(BlockState state, BlockView world, BlockPos pos) {
+        return 1f;
     }
 }
